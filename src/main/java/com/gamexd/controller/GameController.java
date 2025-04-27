b0da1387991f4fe634c96fbdced15a439a1f2a9f
Gabriel Henrique <gabriel.melo@poupex.com.br>
2025-04-27 17:14:40 -0300

    imports

--- src/main/java/com/gamexd/controller/GameController.java
+++ src/main/java/com/gamexd/controller/GameController.java
@@ -5,7 +5,10 @@ import com.gamexd.repository.GameRepository;
 import com.gamexd.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
