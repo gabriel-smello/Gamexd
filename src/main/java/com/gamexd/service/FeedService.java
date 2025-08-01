105291e30f3d9213cf6498a56791cfb6b7c5dc20
Gabriel Henrique <gabriel.melo@poupex.com.br>
2025-07-31 23:47:43 -0300

    trash

--- src/main/java/com/gamexd/service/FeedService.java
+++ src/main/java/com/gamexd/service/FeedService.java
@@ -19,10 +19,6 @@ public class FeedService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private ReviewRepository reviewRepository;
-    @Autowired
-    private GameListRepository gameListRepository;
-    @Autowired
     private FeedRepository feedRepository;
     @Autowired
     private FeedMapper feedMapper;
@@ -39,35 +35,4 @@ public class FeedService {
                 })
                 .toList();
     }
-
-//    public List<FeedDto> getFeed(Jwt jwt) {
-//        User currentUser = userRepository.getReferenceById(Long.valueOf(jwt.getSubject()));
-//
-//        Set<User> following = currentUser.getFollowing();
-//        List<FeedDto> feed = new ArrayList<>();
-//
-//        List<Review> reviews = reviewRepository.findByUserInAndVisibilityOrderByCreatedAtDesc(following, Visibility.PUBLIC);
-//        for (Review r : reviews) {
-//            FeedDto item = new FeedDto();
-//            item.setType("review");
-//            item.setUsername(r.getUser().getEmail());
-//            item.setDescription("avaliou " + r.getGame().getName() + " com " + r.getRating() + " estrelas");
-//            item.setTimestamp(r.getCreatedAt());
-//            feed.add(item);
-//        }
-//
-//        List<GameList> lists = gameListRepository.findByUserInAndVisibilityOrderByCreatedAtDesc(following, Visibility.PUBLIC);
-//        for (GameList list : lists) {
-//            FeedDto item = new FeedDto();
-//            item.setType("list");
-//            item.setUsername(list.getUser().getEmail());
-//            item.setDescription("criou a lista '" + list.getName() + "'");
-//            item.setTimestamp(list.getCreatedAt());
-//            feed.add(item);
-//        }
-//
-//        feed.sort((a, b) -> b.getTimestamp().compareTo(a.getTimestamp()));
-//
-//        return feed;
-//    }
 }
